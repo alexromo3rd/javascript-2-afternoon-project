5adf48df43b498e494df3c314bec355571199c52
Alex Romo III <alexromo3rd@gmail.com>
2020-10-22 19:33:58 -0700

    callbacks complete

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb) {
+  cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,10 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb) {
+  cb(arr[arr.length - 1]);
+}
+
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +73,10 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb) {
+  cb(num1 * num2);
+}
+
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +96,14 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, cb) {
+  if (arr.includes(name)) {
+    cb(true);
+  } else {
+    cb(false);
+  }
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +125,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr.includes(arr[i]) === false) {
+      newArr.push(arr[i]);
+    }
+  }
+  cb(newArr);
+}
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +152,12 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(namesArr, cb) {
+  for (name in namesArr) {
+    cb(name, namesArr.indexOf(name));
+  }
+}
+
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +175,14 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, cb) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === id) {
+      cb(users[i]);
+    }
+  }
+}
+
 
 // Do not edit the code below.
 var users = [
